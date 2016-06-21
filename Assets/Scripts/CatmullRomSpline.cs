21d815ae524d91199925c06bf96e8c70814908ea
Porrith Suong <psuong95@gmail.com>
2016-06-21 17:39:24 -0400

    Removes Start() and Update()

--- Assets/Scripts/CatmullRomSpline.cs
+++ Assets/Scripts/CatmullRomSpline.cs
@@ -3,23 +3,15 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 
-public class CatmullRomSpline : MonoBehaviour {
+public class CatmullRomSpline : MonoBehaviour
+{
 
 	public float sphereRadius = 0.3f;
 	public bool isLooping = true;
 	public Transform[] controlPoints;
 
-
-	private void Start () {
-
-	}
-
-	// Update is called once per frame
-	private void Update () {
-
-	}
-
-	private void OnDrawGizmos() {
+	private void OnDrawGizmos ()
+	{
 		Gizmos.color = Color.white;
 		for (var index = 0; index < controlPoints.Length; index++) {
 			Gizmos.DrawWireSphere (controlPoints [index].position, sphereRadius);
@@ -33,7 +25,8 @@ public class CatmullRomSpline : MonoBehaviour {
 		}
 	}
 
-	private Vector3 GetCatmullRomPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) {
+	private Vector3 GetCatmullRomPoint (float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+	{
 		float half = 0.5f;
 		var point1 = half * (2f * p1);
 		var point2 = half * (p2 - p0);
@@ -45,38 +38,39 @@ public class CatmullRomSpline : MonoBehaviour {
 		return position;
 	}
 
-	private void DisplayCatmullRollSpline(int index) {
-		var p0 = controlPoints[ClampListPos(index - 1)].position;
-		var p1 = controlPoints[index].position;
-		var p2 = controlPoints[ClampListPos(index + 1)].position;
-		var p3 = controlPoints[ClampListPos(index + 2)].position;
+	private void DisplayCatmullRollSpline (int index)
+	{
+		var p0 = controlPoints [ClampListPos (index - 1)].position;
+		var p1 = controlPoints [index].position;
+		var p2 = controlPoints [ClampListPos (index + 1)].position;
+		var p3 = controlPoints [ClampListPos (index + 2)].position;
 
 
 		var lastPosition = Vector3.zero;
 
 		for (float t = 0; t < 1; t += 0.1f) {
-			var newPosition = GetCatmullRomPoint(t, p0, p1, p2, p3);
+			var newPosition = GetCatmullRomPoint (t, p0, p1, p2, p3);
 
 			if (t == 0) {
 				lastPosition = newPosition;
 				continue;
 			}
 
-			Gizmos.DrawLine(lastPosition, newPosition);
+			Gizmos.DrawLine (lastPosition, newPosition);
 			lastPosition = newPosition;
 		}
 
-		Gizmos.DrawLine(lastPosition, p2);
+		Gizmos.DrawLine (lastPosition, p2);
 	}
 
-	private int ClampListPos(int position) {
+	private int ClampListPos (int position)
+	{
 		if (position < 0) {
 			position = controlPoints.Length - 1;
 		}
 		if (position > controlPoints.Length) {
 			position = 1;
-		} 
-		else if (position > controlPoints.Length - 1) {
+		} else if (position > controlPoints.Length - 1) {
 			position = 0;
 		}
 		return position;
